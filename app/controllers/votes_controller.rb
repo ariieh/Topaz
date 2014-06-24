39eb57f6595bbc0f04f3a60f9fe36926eda57696
App Academy Student <student@appacademy.io>
2014-06-24 17:45:17 -0400

    Fix heroku bug attempt1

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,13 @@
 class VotesController < ApplicationController
   def create
-    @vote = Vote.new(author_id: current_user.id, )
-  end
-  
-  private
-  
-  def vote_params
-    params.require(:votes).permit(:)
+    @article = Article.find(params[:article_id])
+    @vote = Vote.new(user_id: current_user.id, article_id: params[:article_id])
+    
+    if @vote.save
+      redirect_to @article
+    else
+      flash[:errors] = @vote.errors.full_messages
+      redirect_to @article
+    end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   validates :article, :user, presence: true
-  validates :article, uniqueness: { scope: :user_id }
+  validates :article, uniqueness: { scope: :user_id, message: "You've already favorited that!" }
   
   belongs_to :article, inverse_of: :votes
   belongs_to :user, inverse_of: :votes
